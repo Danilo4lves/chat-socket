f880691733ca6547725c8d6c5b1c9b296e798219
Danilo Alves <danilo.alves@kinvo.com.br>
2021-08-29 15:22:27 -0300

    feat(App): Fetchs messages on start

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import io from 'socket.io-client';
 
+import { MessageService } from '../services';
+
 import { Container, Messages, Form } from './styles';
 
 const socket = io.connect('/');
@@ -10,6 +12,22 @@ function App() {
   const { register, handleSubmit } = useForm();
 
   const [messages, setMessages] = React.useState([])
+
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    async function fetchMessages() {
+      setIsLoading(true);
+
+      const response = await MessageService.getAll();
+
+      setMessages(response.data.messages);
+
+      setIsLoading(false);
+    }
+
+    fetchMessages();
+  }, []);
 
   React.useEffect(() => {
     socket.on('message', (message) => {
@@ -23,14 +41,24 @@ function App() {
     socket.emit('message', message);
   }
 
-  return (
-    <Container>
+  function renderMessages() {
+    if (isLoading) {
+      return <span>Carregando...</span>;
+    }
+
+    return (
       <Messages>
         {/* TODO: Remove usage of index */}
         {messages.map((message, index) =>
           <li key={`${message}-${index}`}>{message}</li>
         )}
       </Messages>
+    );
+  }
+
+  return (
+    <Container>
+      {renderMessages()}
 
       <Form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('message')} />
